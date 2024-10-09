288796d4da5ff2cd45ac44054210146ccdf38fa2
Pitters Nicolaus Tulungen <nicolauzpitterz@gmail.com>
2024-10-09 17:28:32 +0800

    submit: problem two sum

---
+++ Two Sum.js
@@ -0,0 +1,18 @@
+var twoSum = function (nums, target) {
+  // Solution 1 (Brute Force)
+  // for (let i = 0; i < nums.length; i++) {
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] + nums[j] === target) return [nums[i], nums[j]];
+  //   }
+  // }
+
+  // Solution 2 (HashMap)
+  const numMap = {};
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (numMap[complement] !== undefined) return [numMap[complement], i];
+
+    numMap[nums[i]] = i;
+  }
+};
